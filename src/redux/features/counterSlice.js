2835da9822d542f5ef0cbf9d922f56e9229b5106
zisanurhaque <zisanurhaqueag@gmail.com>
2023-05-25 20:38:27 +0600

    rtk-query-initialized

---
+++ src/redux/features/counterSlice.js
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: 0,
+};
+
+export const counter = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    reset: () => initialState,
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+    decrementByAmount: (state, action) => {
+      state.value -= action.payload;
+    },
+  },
+});
+
+export const {
+  increment,
+  incrementByAmount,
+  decrement,
+  decrementByAmount,
+  reset,
+} = counter.actions;
+export default counter.reducer;
---
+++ src/redux/store.js
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/counterSlice";
+import { userApi } from "./services/userApi";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
+
+export const store = configureStore({
+  reducer: {
+    counterReducer,
+    [userApi.reducerPath]: userApi.reducer,
+  },
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({}).concat([userApi.middleware]),
+});
+
+setupListeners(store.dispatch);
